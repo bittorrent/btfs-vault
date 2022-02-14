4d2154dd66de27cc9c2367a37cb32b4c1bf89db3
ken.sun <ken.sun@tron.network>
2022-02-14 18:43:10 +0800

    Enable admin call logic contract function

--- contracts/VaultProxy.sol
+++ contracts/VaultProxy.sol
@@ -121,7 +121,7 @@ contract VaultProxy is ERC1967Proxy {
      * @dev Makes sure the admin cannot access the fallback function. See {Proxy-_beforeFallback}.
      */
     function _beforeFallback() internal virtual override {
-        require(msg.sender != _getAdmin(), "TransparentUpgradeableProxy: admin cannot fallback to proxy target");
+        //require(msg.sender != _getAdmin(), "TransparentUpgradeableProxy: admin cannot fallback to proxy target");
         super._beforeFallback();
     }
 }
